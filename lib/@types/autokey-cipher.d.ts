549418fc824efd93e2252413430dae393c67f41b
Jose A <jose-pr@coqui.dev>
2020-05-31 20:17:01 +0300

    Update

--- lib/@types/autokey-cipher.d.ts
+++ lib/@types/autokey-cipher.d.ts
@@ -2,3 +2,4 @@
 export declare function Encrypt(plaintext: Buffer): Buffer;
 export declare function EncryptInPlace(plaintext: Buffer): Buffer;
 export declare function DencryptInPlace(ciphertext: Buffer): Buffer;
+export declare function Decrypt(ciphertext: Buffer): Buffer;
